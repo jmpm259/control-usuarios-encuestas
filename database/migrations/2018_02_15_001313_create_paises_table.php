91cde1637c05ce6c2bd2882b326bade38f87f336
Jose Perez <jmpm259@gmail.com>
2018-03-07 12:57:56 -0400

    modificacion en la tabla paises

--- database/migrations/2018_02_15_001313_create_paises_table.php
+++ database/migrations/2018_02_15_001313_create_paises_table.php
@@ -15,7 +15,7 @@ class CreatePaisesTable extends Migration
     {
         Schema::create('paises', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('pais',50);
+            $table->string('pais',50)->unique();
             $table->timestamps();
         });
     }
